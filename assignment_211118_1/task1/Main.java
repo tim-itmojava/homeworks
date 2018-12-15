73c1167f3dd94f4ff4dbe27d2bdecb7b5f390a65
timS <letters.tim@gmail.com>
2018-12-15 23:10:20 +0300

    SplittingFiles

--- assignment_211118_1/task1/Main.java
+++ assignment_211118_1/task1/Main.java
@@ -20,25 +20,12 @@ public class Main {
         // making use of a static method - CopyingFileIoStreams.streamingBytes(File file, String destination, int buffer)
         CopyingFileIoStreams.streamingBytes(source,path,2048*5);
 
-//        source = aTask.defineFile();
-//        path = aTask.defineDestinationPath();
-
-//        System.out.println(Arrays.toString(HandlingFiles.defineHowToSplit(source)));
-
         // method signature for public static String[] splittingFile(int[] size, File file, String destination, int buffer)
-        CopyingFileIoStreams.splittingFile(HandlingFiles.defineHowToSplit(source), source, path, 2048);
+        String[] fileNames = CopyingFileIoStreams.
+                                        splittingFile(HandlingFiles.defineHowToSplit(source), source, path, 2048);
 
-/*
-
-
-        // Splitting the file designated earlier as the copy of source
-        String[] fileNames;
-        fileNames = CopyCmd.SplittingFile(CopyCmd.defineHowToSplit());
-
-        // Reconstructing the parts back into whole
-        GluingFiles restoredFile = new GluingFiles(fileNames,2048);
-        File file = restoredFile.recreateFile();
-**/
+        // method signature for recreateFile(String[] fileNames, int buffer)
+        HandlingFiles.recreateFile(fileNames, 2048);
 
     }
 }
